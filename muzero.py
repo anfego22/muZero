94ad5deb24a34c8cbf15bcfcd3ae7dd03c109e3c
Andres Gonzalez <anfego22@gmail.com>
2023-01-16 15:57:37 -0500

    Add train batch and tests

--- muzero.py
+++ muzero.py
@@ -1,12 +1,16 @@
 import torch
+import torch.nn as nn
+from torch.optim import Adam
 from models import Representation, Dynamics, Prediction
 import utils as ut
 from numpy.random import choice
 from math import log, sqrt
+from typing import Union
 
 
-class Muzero():
+class Muzero(nn.Module):
     def __init__(self, config: dict):
+        super().__init__()
         self.config = config
         self.repInp = (config["observation_dim"][0] + 1) * \
             config["observation_history"]
@@ -24,16 +28,20 @@ class Muzero():
                         [-1], self.dynInp[1], self.dynInp[2]]
         self.f = Prediction(
             self.predInp, config["prediction_hidden_size"] + [config["action_space"]])
+        self.optimizer = Adam(self.parameters(
+        ),  lr=config["adam_lr"], weight_decay=config["adam_weight_decay"])
+        self.eval()
 
-    def dynamics_net(self, obs: torch.Tensor, act: int):
+    def dynamics_net(self, obs: torch.Tensor, act: Union[int, list[int]]):
         act = ut.action_to_plane(act, dim=self.dynInp[1:])
         dynInp = torch.cat([obs, act], 1)
         return self.g(dynInp)
 
     def puct_score(self, parent: ut.Node, node: ut.Node):
+        visits = sum([n.countVisits for n in parent.children.values()])
         result = self.config["pUCT_score_c1"] + log(
-            (parent.countVisits + self.config["pUCT_score_c2"] + 1) / self.config["pUCT_score_c2"])
-        result *= node.prob*sqrt(parent.countVisits) / (1 + node.countVisits)
+            (visits + self.config["pUCT_score_c2"] + 1) / self.config["pUCT_score_c2"])
+        result *= node.prob*sqrt(visits) / (1 + node.countVisits)
         result += node.get_value()
         return result
 
@@ -55,35 +63,61 @@ class Muzero():
 
         Root node values.
         """
-        root = ut.Node(0)
-        root.hiddenState = self.h(obs)
-        root.countVisits += 1
-        probs, _ = self.f(root.hiddenState)
-        for i, p in enumerate(probs):
-            root.children[i] = ut.Node(p)
-
-        for _ in range(nSimul):
-            history = [root]
-            node = history[0]
-            # While node is expanded, traverse the tree until reach a leaf node and expanded.
-
-            while len(node.children) > 0:
-                act, node = self.select_action(node)
-                history.append(node)
-
-            parent = history[-2]
-            node.hiddenState, node.reward = self.dynamics_net(
-                parent.hiddenState, act)
-            probs, value = self.f(node.hiddenState)
-
+        with torch.no_grad():
+            root = ut.Node(0)
+            root.hiddenState = self.h(obs)
+            probs, _ = self.f(root.hiddenState)
             for i, p in enumerate(probs):
-                node.children[i] = ut.Node(p)
+                root.children[i] = ut.Node(p)
 
-            for n in reversed(history):
-                n.countVisits += 1
-                n.totalValue += value
-                value = n.reward + self.config["mcts_discount_value"]*value
+            for _ in range(nSimul):
+                history = [root]
+                node = history[0]
+                # While node is expanded, traverse the tree until reach a leaf node and expanded.
 
-        policy = [n.countVisits /
-                  root.countVisits for n in root.children.values()]
-        return policy, root.get_value()
+                while len(node.children) > 0:
+                    act, node = self.select_action(node)
+                    history.append(node)
+
+                parent = history[-2]
+                node.hiddenState, node.reward = self.dynamics_net(
+                    parent.hiddenState, act)
+                probs, value = self.f(node.hiddenState)
+
+                for i, p in enumerate(probs):
+                    node.children[i] = ut.Node(p)
+
+                for n in reversed(history):
+                    n.countVisits += 1
+                    n.totalValue += value
+                    value = n.reward + self.config["mcts_discount_value"]*value
+
+            policy = [n.countVisits for n in root.children.values()]
+            norm = sum(policy)
+            policy = [p / norm for p in policy]
+            return policy, root.get_value()
+
+    def train_batch(self, batch: dict) -> None:
+        """Train the model.
+
+        batch: A list of dicts, each dict represent a batch of action in step k
+            batch[0] = {obs     : torch.Tensor size(batchSize, (channel + 1)*prevObs, H, W)
+                        act     : list[int]    size(batchSize, 1)
+                        rew     : torch.Tensor size(batchSize, 1)
+                        val     : torch.Tensor size(batchSize, 1)
+                        policy  : torch.Tensor size(batchSize, actionSpace)
+                        }
+        """
+        s = self.h(batch[0]["obs"])
+        loss1, loss2 = nn.BCEWithLogitsLoss(), nn.MSELoss()
+        totalLoss = 0
+        for b in batch:
+            s, r = self.dynamics_net(s, b["act"])
+            p, v = self.f(s)
+            totalLoss += loss1(p, b["pol"])
+            totalLoss += loss2(v, b["val"])
+            totalLoss += loss2(r, b["rew"])
+        self.optimizer.zero_grad()
+        totalLoss.backward()
+        self.optimizer.step()
+        self.eval()
--- test_muzero.py
+++ test_muzero.py
@@ -1,5 +1,6 @@
 from muzero import Muzero
 import torch
+import utils as ut
 
 config = {
     "observation_dim": [3, 64, 64],
@@ -11,6 +12,8 @@ config = {
     "mcts_discount_value": 0.8,
     "pUCT_score_c1": 1.25,
     "pUCT_score_c2": 19652,
+    "adam_weight_decay": 1e-5,
+    "adam_lr": 1e-4,
 }
 
 # TODO: Make it work with high and width that are multiple of two.
@@ -40,3 +43,41 @@ def test_prediction_shapes():
     p, v = mz.f(dyn)
     assert p.shape == (1, 3)
     assert v.shape == (1, 1)
+
+
+def test_puct_score():
+    c = config.copy()
+    mz = Muzero(c)
+    node = ut.Node(0)
+    node.children = {i: ut.Node((i+1)/5) for i in range(5)}
+    res = mz.puct_score(node, node.children[0])
+    assert res == 0
+
+
+def test_train():
+    c = config.copy()
+    mz = Muzero(c)
+    batch = [
+        {
+            "obs": torch.Tensor(2, (3 + 1)*3, 64, 64),
+            "act": [1, 2],
+            "rew": torch.Tensor([0, 0]),
+            "val": torch.Tensor([-1, -1]),
+            "pol": torch.Tensor(2, 3),
+        },
+        {
+            "obs": torch.Tensor(2, (3 + 1)*3, 64, 64),
+            "act": [1, 2],
+            "rew": torch.Tensor([0, 0]),
+            "val": torch.Tensor([-1, -1]),
+            "pol": torch.Tensor(2, 3),
+        },
+        {
+            "obs": torch.Tensor(2, (3 + 1)*3, 64, 64),
+            "act": [1, 2],
+            "rew": torch.Tensor([0, 0]),
+            "val": torch.Tensor([-1, -1]),
+            "pol": torch.Tensor(2, 3),
+        }
+    ]
+    mz.train_batch(batch)
