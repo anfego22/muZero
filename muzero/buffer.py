617fd3c722bee654e9ffac4284864424a6b2d252
Andres Gonzalez <anfego22@gmail.com>
2023-01-25 22:32:18 -0500

    Fix make_obs

--- muzero/buffer.py
+++ muzero/buffer.py
@@ -46,7 +46,7 @@ class ReplayBuffer(object):
         batch = []
         for s in range(steps + 1):
             batch.append({
-                "obs": torch.concat([self.make_obs(selGame, p+s) for p in pos], 0),
+                "obs": torch.concat([self.make_obs(selGame, p+s, prev_obs) for p in pos], 0),
                 "act": [selGame[p+s]["act"] for p in pos],
                 "rew": torch.Tensor([selGame[p+s]["rew"] for p in pos]),
                 "pol": torch.stack([selGame[p+s]["pol"] for p in pos]),
